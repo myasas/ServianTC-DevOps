e1ea263f837fb286cdb200247b4fe87fc1b9c53c
Yasas De Silva <myasas@gmail.com>
2021-09-16 06:12:04 +0530

    Update variables

--- terraform/infrastructure/variables.tf
+++ terraform/infrastructure/variables.tf
@@ -4,3 +4,28 @@ variable "eks_version" {
 
 variable "vpc_cidr" {
 }
+
+variable "vpc_subnet_public_cidr" {
+  type = list(string)
+}
+
+variable "vpc_subnet_private_cidr" {
+  type = list(string)
+}
+
+variable "vpc_az_size" {
+}
+
+
+
+variable "bastion_allowed_port" {
+  type = string
+}
+
+variable "bastion_allowed_cidrs" {
+  type = list(string)
+}
+
+variable "nat_gateway_size" {
+  type = string
+}
