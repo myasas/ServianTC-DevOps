0d9a6e13a071141a0c3ab8e16c275a5acbecca5f
Yasas De Silva <myasas@gmail.com>
2021-09-16 06:45:45 +0530

    Add RDS resource details for security

---
+++ terraform/infrastructure/modules/security/storage.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "storage" {
+  name   = "${var.short_name}-storage-sg"
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.default_tags, {
+    Name = "${var.short_name}-storage-sg"
+  })
+
+  description = "Auto assigned by code."
+}
+
+resource "aws_security_group_rule" "storage_ingress_22" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = var.vpc_subnet_private_cidr
+
+  security_group_id = aws_security_group.storage.id
+
+  description = "Auto assigned by code."
+}
+
+resource "aws_security_group_rule" "storage_ingress_2049" {
+  type        = "ingress"
+  from_port   = 2049
+  to_port     = 2049
+  protocol    = "tcp"
+  cidr_blocks = var.vpc_subnet_private_cidr
+
+  security_group_id = aws_security_group.storage.id
+
+  description = "Auto assigned by code."
+}
+
+resource "aws_security_group_rule" "storage_ingress_5432" {
+  type        = "ingress"
+  from_port   = 5432
+  to_port     = 5432
+  protocol    = "tcp"
+  cidr_blocks = var.vpc_subnet_private_cidr
+
+  security_group_id = aws_security_group.storage.id
+
+  description = "Auto assigned by code."
+}
