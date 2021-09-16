92597b307190644d685fe7e176438aec6cb57991
Yasas De Silva <myasas@gmail.com>
2021-09-16 05:59:45 +0530

    Add module network/vpc/ subnets_v2

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -11,3 +11,19 @@ required_providers {
     }
   }
 }
+
+module "network" {
+  source = "./modules/network"
+
+  short_name   = var.short_name
+  default_tags = var.default_tags
+
+  vpc_cidr    = var.vpc_cidr
+  vpc_az_size = var.vpc_az_size
+
+  nat_gateway_size = var.nat_gateway_size
+
+  vpc_subnet_public_cidr  = var.vpc_subnet_public_cidr
+  vpc_subnet_private_cidr = var.vpc_subnet_private_cidr
+  vpc_subnet_storage_cidr = var.vpc_subnet_storage_cidr
+}
