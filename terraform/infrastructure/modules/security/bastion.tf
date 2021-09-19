d70fb487ddde1d80c2ced4e4ba3285f4cce8a2b1
Yasas De Silva <myasas@gmail.com>
2021-09-19 20:42:23 +0530

    Makesure terraform execution environment is whitelisted in bastion security groups

--- terraform/infrastructure/modules/security/bastion.tf
+++ terraform/infrastructure/modules/security/bastion.tf
@@ -1,3 +1,7 @@
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
 resource "aws_security_group" "bastion" {
   name   = "${var.short_name}-bastion-sg"
   vpc_id = var.vpc_id
@@ -21,7 +25,7 @@ resource "aws_security_group_rule" "bastion_ingress_ssh" {
   from_port   = var.bastion_allowed_port
   to_port     = var.bastion_allowed_port
   protocol    = "tcp"
-  cidr_blocks = var.bastion_allowed_cidrs
+  cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
 
   security_group_id = aws_security_group.bastion.id
 
